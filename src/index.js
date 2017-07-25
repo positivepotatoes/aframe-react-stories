d6fffcbbf30cf722f993efa513b935b3b1e65f1e
Alexander Schnapp <alexanderschnapp@gmail.com>
2017-07-24 17:06:25 -0700

    Create starter

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-class VRStories extends React.Component {
-  render() {
-    return (
-      <div>insert VR here</div>
-    );
-  }
-}
-export default VRStories;
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,21 +1,22 @@
+var webpack = require('webpack');
 var path = require('path');
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    libraryTarget: 'commonjs2' // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+    libraryTarget: 'commonjs2'
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|bower_components|build)/,
+        exclude: ['node_modules'],
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['react', 'es2015']
           }
         }
       }
