d3def38583d14b9454b911c468cfeb2f10728ef6
David Oh <davidjohis@gmail.com>
2017-07-26 16:12:35 -0700

    get play button listening to events

--- src/VRNext.jsx
+++ src/VRNext.jsx
@@ -3,31 +3,37 @@ import 'aframe-animation-component';
 
 const VRNext = (props) => {
 
-  let animation__y = 'property: rotation; dir: alternate; dur: 800; easing: easeInSine; loop: true; from: 0 0 1.5; to: 0 0 -1.5';
-  let animation__x;
+
 
   const clickPlay = () => {
-    console.log('u clicked');
-    animation__y = 'property: rotation; dir: alternate; dur: 800; easing: easeInSine; loop: true; from: 90 0 0; to: -90 0 0';
-    // animation__y = null;
     props.playNext();
+    // animation__y = 'property: rotation; dir: alternate; dur: 800; easing: easeInSine; loop: true; from: 90 0 0; to: -90 0 0';
+    // animation__y = null;
+    // console.log(document.getElementById('playnextbutton'))
+    
+    // document.getElementById('playnextbutton').emit('initializeplay')
+
   }
 
   return (
-    <a-entity position='0 0 -3' rotation='0 0 270'>
+    <a-entity position='0 0 -3' rotation='0 0 270' >
       
       <a-cone 
+      id='playnextbutton'
         color='#bcbcbc'
         // metalness='.9'
         onClick={clickPlay}
-        radius-top='0'
-        radius-bottom='.5'
+        height='.4'
+        radius-top='.01'
+        radius-bottom='.24'
         material='transparent: true; opacity: .6'
         // animation__x='property: rotation; dir: alternate; dur: 800; easing: easeInSine; loop: true; to: 40 0 0'
-        animation__y={animation__y}
+        animation__y='property: rotation; dir: alternate; dur: 800; easing: easeInSine; loop: true; from: 0 0 2.5; to: 0 0 -1.5'
+        animation__color='property: color; dir: alternate; dur: 1100; easing: easeInSine; loop: true; to: #8e8e8e'
+
+        animation__playingmove='property: position; dur: 800; easing: easeInOutQuart; repeat: 1; to: 2 0 0; startEvents: initializeplay'
+        animation__playingturn='property: rotation; dur: 800; easing: easeInSine; repeat: 1; from: 0 0 2.5; to: -90 0 0; startEvents: initializeplay'
         // animation__x={animation__x}
-        animation__color="property: color; dir: alternate; dur: 900;
-                             easing: easeInSine; loop: true; to: #8e8e8e"
       >
       </a-cone>
       
