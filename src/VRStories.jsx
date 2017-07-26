48eb576a3f9297a0563fcb4761baa068062a7c34
Alexander Schnapp <alexanderschnapp@gmail.com>
2017-07-26 11:09:04 -0700

    Comment out exit (unfinished)

--- src/VRStories.jsx
+++ src/VRStories.jsx
@@ -273,7 +273,7 @@ class VRStories extends React.Component {
         />
 
         <VRPrimitive currentStory={currentStory}/>
-        <VRExit exitCallback={this.props.exitCallback}/>
+        {/* <VRExit exitCallback={this.props.exitCallback}/> */}
       </a-entity>
     );
   }
